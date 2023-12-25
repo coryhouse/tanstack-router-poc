c01986e69697a8238535868927de324dfe237b96
Cory House <housecor@gmail.com>
2023-12-25 07:40:20 -0600

    add mocking

--- src/main.tsx
+++ src/main.tsx
@@ -11,6 +11,16 @@ import {
   RouterProvider,
 } from "@tanstack/react-router";
 
+async function enableMocking() {
+  if (import.meta.env.PROD) return;
+
+  const { worker } = await import("./mocks/browser");
+
+  // `worker.start()` returns a Promise that resolves
+  // once the Service Worker is up and ready to intercept requests.
+  return worker.start();
+}
+
 const TanStackRouterDevtools = import.meta.env.PROD
   ? () => null // Render nothing in production
   : React.lazy(() =>
@@ -112,8 +122,10 @@ declare module "@tanstack/react-router" {
   }
 }
 
-ReactDOM.createRoot(document.getElementById("root")!).render(
-  <React.StrictMode>
-    <RouterProvider router={router} />
-  </React.StrictMode>
-);
+enableMocking().then(() => {
+  ReactDOM.createRoot(document.getElementById("root")!).render(
+    <React.StrictMode>
+      <RouterProvider router={router} />
+    </React.StrictMode>
+  );
+});
