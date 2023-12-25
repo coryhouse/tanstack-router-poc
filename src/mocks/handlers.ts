7b5c963b2a0b8fc0b3b2d05d8576a3cdd175007f
Cory House <housecor@gmail.com>
2023-12-25 07:36:54 -0600

    add msw

--- src/mocks/handlers.ts
+++ src/mocks/handlers.ts
@@ -6,12 +6,33 @@ type Product = {
   price: number;
 };
 
+const products: Product[] = [
+  { id: 1, name: "Product 1", price: 100 },
+  { id: 2, name: "Product 2", price: 200 },
+];
+
 export const handlers = [
   http.get("/products", () => {
-    const products: Product[] = [
-      { id: 1, name: "Product 1", price: 100 },
-      { id: 2, name: "Product 2", price: 200 },
-    ];
+    return HttpResponse.json(products);
+  }),
+
+  http.delete("/products/:id", ({ params }) => {
+    const { id } = params;
+    products.splice(parseInt(id.toString()), 1);
+    return HttpResponse.json(products);
+  }),
+
+  http.post("/products", async ({ request }) => {
+    const { name, price } = (await request.json()) as Omit<Product, "id">;
+    products.push({ id: products.length + 1, name, price });
+    return HttpResponse.json(products);
+  }),
+
+  http.put("/products/:id", async ({ request }) => {
+    const product = (await request.json()) as Product;
+    products.forEach((p) => {
+      if (p.id === product.id) p = product;
+    });
 
     return HttpResponse.json(products);
   }),
